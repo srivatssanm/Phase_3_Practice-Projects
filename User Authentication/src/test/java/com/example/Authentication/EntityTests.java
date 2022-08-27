a6fc01ae6a68549f5c738b7f7bd94fd5cc44345c
Srivatssan <94544700+srivatssanm@users.noreply.github.com>
2022-08-27 17:31:53 +0530

    Phase3_PracticeProject_3

---
+++ User Authentication/src/test/java/com/example/Authentication/EntityTests.java
@@ -0,0 +1,66 @@
+package com.example.Authentication;
+
+
+import com.example.Authentication.entities.User;
+import com.example.Authentication.repositories.UserRepository;
+import com.example.Authentication.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class EntityTests {
+
+	@Test
+	public void getAndSetPassword() {
+		User testUser = new User();
+		
+		testUser.setPassword("mypassword");
+		assertEquals(testUser.getPassword(),"mypassword");
+	}
+	
+	@Test
+	public void getAndSetName() {
+		User testUser = new User();
+		
+		testUser.setName("Sri");
+		assertEquals(testUser.getName(),"Sri");
+	}
+	
+	@Test
+	public void getAndSetEmail() {
+		User testUser = new User();
+		
+		testUser.setEmail("Sri@email.com");
+		assertEquals(testUser.getEmail(),"Sri@email.com");
+	}
+	
+	@Test
+	public void checkToString() {
+		User testUser = new User();
+		
+		assertNotNull(testUser.toString());
+	}
+	
+	@Test
+	public void checkConstructor() {
+		User testUser = new User("Sri","Sri@email.com","0987");
+		
+		User checkUser = new User();
+		checkUser.setName("Sri");
+		checkUser.setEmail("Sri@email.com");
+		checkUser.setPassword("0987");
+		
+		assertEquals(testUser.getName(), checkUser.getName());
+		assertEquals(testUser.getEmail(), checkUser.getEmail());
+		assertEquals(testUser.getPassword(), checkUser.getPassword());
+	}
+	
+	@Test
+	public void testDefaultConstructor() {
+		User testUser = new User();
+		
+		assertNotNull(testUser);
+	}
+}
